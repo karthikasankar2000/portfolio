592fbc857c9b26f40d51810b833b4818f1f8238c
karthikasankar2000 <72914339+karthikasankar2000@users.noreply.github.com>
2020-11-05 16:34:10 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,32 @@
+function active1()
+{
+    var act,in1,in2;
+    act=document.getElementById('home');
+    in1=document.getElementById('about');
+    in2=document.getElementById('contact');
+    act.style.color="#ffffff";
+    in1.style.color="#a77ace";
+    in2.style.color="#a77ace";
+}
+
+function active2()
+{
+    var act,in1,in2;
+    act=document.getElementById('about');
+    in1=document.getElementById('home');
+    in2=document.getElementById('contact');
+    act.style.color="#ffffff";
+    in1.style.color="#a77ace";
+    in2.style.color="#a77ace";
+}
+
+function active3()
+{
+    var act,in1,in2;
+    act=document.getElementById('contact');
+    in1=document.getElementById('about');
+    in2=document.getElementById('home');
+    act.style.color="#ffffff";
+    in1.style.color="#a77ace";
+    in2.style.color="#a77ace";
+}
